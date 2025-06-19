1a893e0358cc11a18ced647258e0719870db9594
EC2 Default User <ec2-user@ip-172-16-124-19.ec2.internal>
2025-06-19 18:19:58 +0000

    removed redundant files

--- evaluate.py
+++
@@ -1,9 +0,0 @@
-from preprocessing import evaluate_model
-
-def main():
-    print("🧪 Starting model evaluation...")
-    evaluate_model.evaluate_model()
-    print("✅ Evaluation complete.")
-
-if __name__ == "__main__":
-    main()
